5d8a2eb70072e6efa175612967fee7de50231ae7
Flesha Andhariya <fleshaandhariya1701@gmail.com>
2026-09-17 10:20:59 +0530

    Update JS slider

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@ const track = document.querySelector("#sliderTrack");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 let logos = [...document.querySelectorAll(".logo")];
-let moving = false;
 function slide(direction) {
-    if (moving) return;
-    moving = true;
     track.style.transition = "0.5s";
     track.style.transform = direction == "next"
         ? "translateX(-25%)"
@@ -17,7 +14,6 @@ function slide(direction) {
         logos.forEach(logo => track.appendChild(logo));
         track.style.transition = "none";
         track.style.transform = "translateX(0)";
-        moving = false;
     }, 500);
 }
 next.onclick = () => slide("next");
